194cacfc1b6db2b3182c9bdfaebcccb68fd447dd
zheng <zhengrongbyron@gmail.com>
2022-08-11 01:39:04 -0700

    update from win10

--- reactpractice2022/src/App.js
+++ reactpractice2022/src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
+
 import './App.css';
 import ClickCount from './components/ClickCount';
 import HoverCount from './components/HoverCount';
+import React  from 'react';
 
 function App() {
   return (
