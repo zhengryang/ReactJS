bbff1a6dda29865fe952d799eb4efc5e0b321432
zhengryang <zhengrongbyron@gmail.com>
2022-10-09 23:59:04 -0700

    56 - fetching data 2 - get specific post by passing id to get request

--- state-hook/src/components/FetchingData.js
+++ state-hook/src/components/FetchingData.js
@@ -3,28 +3,31 @@ import axios from 'axios'
  
 const DataFetching = () => {
  
-   const [posts, setPosts] = useState([])
+   const [post, setPost] = useState({})
+   const [id, setId] = useState(1)
  
    useEffect (()=>{
-       axios.get('https://jsonplaceholder.typicode.com/posts')
+       axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
        .then(res => {
            console.log(res)
-           setPosts(res.data)
+           setPost(res.data)
        })
        .catch(err => {
            console.log(err)
        })
-   })
+   }, [id])
  
    return (
        <div>
-           <ul>
+        <input type = "text" value = {id} onChange = {e => setId(e.target.value)}></input>
+        <div>{post.title}</div>
+           {/* <ul>
                {
                    posts.map(post => (
                        <li key = {post.id}> {post.title}</li>
                    ))
                }
-           </ul>
+           </ul> */}
        </div>
    );
 };
