9348a3e7eb7c9cd87ef2a9f00e97c1133f70468e
zhengryang <zhengrongbyron@gmail.com>
2022-10-10 09:32:35 -0700

    57 - fetching data 3 - button trigger get request

--- state-hook/src/components/FetchingData.js
+++ state-hook/src/components/FetchingData.js
@@ -5,9 +5,14 @@ const DataFetching = () => {
  
    const [post, setPost] = useState({})
    const [id, setId] = useState(1)
+   const [idFromButtonClick, setIdFromButtonClick] = useState(1)
  
+    const handleClick = () => {
+        setIdFromButtonClick(id);
+    }
+
    useEffect (()=>{
-       axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+       axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
        .then(res => {
            console.log(res)
            setPost(res.data)
@@ -15,11 +20,12 @@ const DataFetching = () => {
        .catch(err => {
            console.log(err)
        })
-   }, [id])
+   }, [idFromButtonClick])
  
    return (
        <div>
         <input type = "text" value = {id} onChange = {e => setId(e.target.value)}></input>
+        <button type = "button" onClick={handleClick}>fetch</button>
         <div>{post.title}</div>
            {/* <ul>
                {
